52f7cd8a9818f6d29c7d384d2528b45ec3f67933
Panteve <diegorojer@gmail.com>
2026-03-12 09:49:12 -0500

    feat: agregar componente de movimientos de inventario y sus archivos asociados

---
+++ src/app/features/inventory/layouts/inventory-movement.component/inventory-movement.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inventory-movement.component',
+  imports: [],
+  templateUrl: './inventory-movement.component.html',
+  styleUrl: './inventory-movement.component.css',
+})
+export class InventoryMovementComponent {
+
+}
