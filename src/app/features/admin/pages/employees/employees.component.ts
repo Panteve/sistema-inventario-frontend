9561f9dcce4dbb0784701565ffa7b3f36a5127a8
Panteve <diegorojer@gmail.com>
2026-05-04 11:40:33 -0500

    cambio de nombre de la carpeta

--- src/app/features/admin/pages/employees/employees.component.ts
+++ src/app/features/admin/pages/employees/employees.component.ts
@@ -11,7 +11,7 @@ import {
 import { ToastService } from '../../../../shared/services/toast.service';
 import { EmployeeAction, EmployeeResponse } from '../../../../shared/interfaces/employee.interface';
 import { finalize } from 'rxjs';
-import { ManageEmployeeComponent } from '../../layouts/manage-employee.component/manage-employee.component';
+import { ManageEmployeeComponent } from '../../layouts/manage-employee/manage-employee.component';
 
 @Component({
   selector: 'app-employees.component',
