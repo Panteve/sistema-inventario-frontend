b53a3d5c576cb083c654cd882f35fc8700a6765f
Panteve <diegorojer@gmail.com>
2026-05-05 13:31:44 -0500

    feat: crear componente para  listar gastos, incluyendo estilos y lógica de formulario

--- src/app/shared/interfaces/expense.interface.ts
+++ src/app/shared/interfaces/expense.interface.ts
@@ -2,3 +2,43 @@ export interface CreateExpenseRequest {
   amount: number;
   reason: string;
 }
+
+export interface ExpensePagination {
+  totalItems: number;
+  totalPages: number;
+}
+
+export interface ExpenseResponse {
+  data: Expense[];
+  pagination: ExpensePagination;
+}
+
+export interface Expense {
+  id: number;
+  amount: number;
+  reason: string;
+  office: {
+    id: number;
+    name: string;
+  };
+  employee: {
+    id: number;
+    name: string;
+    document: string;
+  };
+  cashRegister: {
+    id: number;
+    openedAt: string;
+  };
+}
+
+export interface ParamsGetBills {
+  startDate: string;
+  endDate: string;
+  officeId?: number;
+  employeeId?: number;
+  amountMin?: number;
+  amountMax?: number;
+  page: number;
+  limit: number;
+}
