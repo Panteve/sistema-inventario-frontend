1f8c3ebb483c8be72d77bec589fc6292fed2359d
Panteve <diegorojer@gmail.com>
2026-06-24 15:44:21 -0500

    cambios de estilo

--- src/app/constants/theme.constants.ts
+++ src/app/constants/theme.constants.ts
@@ -1,10 +1,10 @@
 export const THEMES = {
   LIGHT: 'light',
   DARK: 'dark',
-  DRACULA: 'dracula',
+  business: 'business',
 } as const;
 
 export type ThemeName = (typeof THEMES)[keyof typeof THEMES];
 
-export const DEFAULT_APP_THEME: ThemeName = THEMES.DRACULA;
-export const FIXED_LAYOUT_THEME: ThemeName = THEMES.DRACULA;
+export const DEFAULT_APP_THEME: ThemeName = THEMES.business;
+export const FIXED_LAYOUT_THEME: ThemeName = THEMES.business;
